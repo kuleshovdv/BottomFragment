d06624733019bbca7a893136ae710dc3401e2a73
Dmitry Kuleshov <info@lineris.ru>
2022-07-01 18:52:08 +0300

    QRHelper parameters

--- bottomfragment/src/main/java/ru/lineris/bottomfragment/QRHelper.kt
+++ bottomfragment/src/main/java/ru/lineris/bottomfragment/QRHelper.kt
@@ -8,22 +8,25 @@ import com.google.zxing.WriterException
 import com.google.zxing.common.BitMatrix
 
 object QRHelper {
-    fun buildQRcode(value: String): Bitmap? {
+    fun buildQRcode(value: String,
+                    size: Int = 400,
+                    mainColor: Int = Color.BLACK,
+                    backgroundColor: Int = Color.WHITE): Bitmap? {
         val multiFormatWriter = MultiFormatWriter()
         return try {
             val bitMatrix = multiFormatWriter.encode(
                 value,
                 BarcodeFormat.QR_CODE,
-                400,
-                400)
-            createBitmap(bitMatrix)
+                size,
+                size)
+            createBitmap(bitMatrix, mainColor, backgroundColor)
         } catch (e: WriterException) {
             e.printStackTrace()
             null
         }
     }
 
-    private fun createBitmap(matrix: BitMatrix): Bitmap {
+    private fun createBitmap(matrix: BitMatrix, mainColor: Int, backgroundColor: Int): Bitmap {
         val width = matrix.width
         val height = matrix.height
         val pixels = IntArray(width * height)
@@ -31,7 +34,7 @@ object QRHelper {
             val offset = y * width
             for (x in 0 until width) {
                 pixels[offset + x] =
-                    if (matrix[x, y]) Color.BLACK else Color.WHITE
+                    if (matrix[x, y]) mainColor else backgroundColor
             }
         }
         val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
